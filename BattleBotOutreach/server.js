2e030896c40257eb597dd1f0bd0a58cf53b7c36b
trakyari <tarijus7@gmail.com>
2021-01-29 21:32:19 -0600

    express work

--- BattleBotOutreach/server.js
+++ BattleBotOutreach/server.js
@@ -1,5 +1,3 @@
-// const http = require('http');
-// const fs = require('fs');
 const hostname = 'localhost';
 const port = 3000;
 
@@ -12,16 +10,10 @@ app.get('/', function(req, res) {
 });
 
 app.get('/Bot1', function(req, res) {
-  res.sendFile(path.join(__dirname + '/Bot1/Bot1.html'));
+  res.sendFile('/Bot1/Bot1.html');
+  // res.sendFile(path.join(__dirname + '/Bot1/Bot1.html'));
 });
 
-/* const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  fs.createReadStream('index.html').pipe(res);
-  res.end('ETC Sumobot');
-});
-*/
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
