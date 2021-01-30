67f1007e5420cd42c086e9eff474f74ea4c6e24d
trakyari <tarijus7@gmail.com>
2021-01-29 19:44:31 -0600

    added express links for multiple paths

--- BattleBotOutreach/server.js
+++ BattleBotOutreach/server.js
@@ -11,6 +11,10 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get('/Bot1', function(req, res) {
+  res.sendFile(path.join(__dirname + '/Bot1/Bot1.html'));
+});
+
 /* const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
