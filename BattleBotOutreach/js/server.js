9bd8aff4414e32129e4f3803b771ee2e3d6924a9
trakyari <35118892+trakyari@users.noreply.github.com>
2021-04-19 21:41:52 -0500

    server.js fix

--- BattleBotOutreach/js/server.js
+++ BattleBotOutreach/js/server.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  fs.createReadStream('../index.html').pipe(res)
+  fs.createReadStream('../index.html').pipe(res);
   res.end('ETC Sumobot');
 });
 
